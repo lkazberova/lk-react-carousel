265fc39f26bffef6eb49ac90573c9f9549016be3
Leisan Kazberova <leisan.kh@gmail.com>
2018-11-15 14:49:09 +0400

    Updates

---
+++ precache-manifest.44129ac7d05a25ed6ef29ee5c4a5bc15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d034246f8c17d03e628",
+    "url": "/lk-react-carousel/static/js/runtime~main.5d034246.js"
+  },
+  {
+    "revision": "5c3f91a39f0bfcd5a435",
+    "url": "/lk-react-carousel/static/js/main.5c3f91a3.chunk.js"
+  },
+  {
+    "revision": "9072979c647380351ea0",
+    "url": "/lk-react-carousel/static/js/1.9072979c.chunk.js"
+  },
+  {
+    "revision": "5c3f91a39f0bfcd5a435",
+    "url": "/lk-react-carousel/static/css/main.b070254d.chunk.css"
+  },
+  {
+    "revision": "cc77b12c639e3e608ef430f10c811bf2",
+    "url": "/lk-react-carousel/index.html"
+  }
+];
